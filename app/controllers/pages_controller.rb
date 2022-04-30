0530f9c982a2c777c1f1e45c103ffbe83e738115
Sherman Fong <sfong.ima@gmail.com>
2022-04-30 17:10:32 -0400

    Small fixes on art piece indexpage and removed duplicate code on cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
+    @artpieces = []
+    3.times do
+      random = Random.new
+      @artpieces << ArtPiece.find(random.rand(1..ArtPiece.all.length))
+    end
   end
 end
