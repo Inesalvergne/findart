15d744805e4d3cf036630e415351874dfdf43b0b
Ines Alvergne <i.alvergne@gmail.com>
2022-05-07 15:58:57 -0400

    you can now create a new artpiece from everywhere on the website

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :new_artpiece
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %I[first_name last_name])
   end
+
+  private
+
+  def new_artpiece
+    @artpiece = ArtPiece.new
+  end
+
 end
