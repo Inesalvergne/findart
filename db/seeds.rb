f460011c17425052645cd6d20b90135d05145dab
Ibrahim Maqsood ninjaibmaq@gmail.com <ninjaibmaq@gmail.com>
2022-05-07 12:21:03 -0400

    Seed-file progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,21 +35,23 @@ puts 'Recreation complete!'
 ArtPiece.create(title: 'David', artist: 'Michelangelo', description: 'David is a masterpiece of Renaissance
   sculpture, created in marble between 1501 and 1504 by the Italian artist Michelangelo. David is a 5.17-metre
    (17 ft 0 in) marble statue of the Biblical figure David, a favoured subject in the art of Florence.',
-creation_date: 'May 1st', category: 'Sculpture', price_rate:  5_000_000, style: 'Renaissance', user: User.rand,
-image_url: "https://en.wikipedia.org/wiki/David_(Michelangelo)#/media/File:Michelangelo's_David_-_right_view_2.jpg"
+creation_date: '1501', category: 'Sculpture', price_rate:  5_000_000, style: 'Renaissance', user: User.rand,
+image_url: "https://en.wikipedia.org/wiki/David_(Michelangelo)#/media/File:Michelangelo's_David_-_right_view_2.jpg")
 
 ArtPiece.create(title: 'Salvator Mundi', artist:'Leonardo da Vinci', description:
 'The painting depicts Christ in an anachronistic blue Renaissance dress,
 making the sign of the cross with his right hand, while holding a transparent,
 non-refracting crystal orb in his left, signaling his role as Salvator Mundi
 and representing the of the heavens.', category: 'Painting', price_rate: '$2,500,000',
- style: 'Renaissance', user: User.rand, image_url: ""https://en.wikipedia.org/wiki/Salvator_Mundi_(Leonardo)#/media/File:Leonardo_da_Vinci,_Salvator_Mundi,_c.1500,_oil_on_walnut,_45.4_%C3%97_65.6_cm.jpg")" )
+style: 'Renaissance', user: User.rand, image_url: "https://en.wikipedia.
+org/wiki/Salvator_Mundi_(Leonardo)#/media/File:Leonardo_da_Vinci,_
+Salvator_Mundi,_c.1500,_oil_on_walnut,_45.4_%C3%97_65.6_cm.jpg")
 
 ArtPiece.create(title: 'The Card Players', artist: 'Paul Cézanne', description: 'The Card Players is
    a series of oil paintings
    by the French Post-Impressionist artist Paul Cézanne. Painted during Cézannes final period in the early 1890s,
   there are five paintings in the series. The versions vary in size,
-   the number of players, and the setting in which the game takes place.' creation_date: 1894 category: 'Painting', price_rate: 2_000_000, style: "Post-Impressionist",
+   the number of players, and the setting in which the game takes place.', creation_date: 1894 category: 'Painting', price_rate: 2_000_000, style: "Post-Impressionist",
   user: User.rand, image_url: "https://en.wikipedia.org/wiki/The_Card_Players#/media/File:Les_Joueurs_de_cartes,_par_Paul_C%C3%A9zanne.jpg")
 
 ArtPiece.create(title: "The Scream", artist: "Edvard Munch", description: "The Scream is the popular
@@ -81,21 +83,30 @@ ArtPiece.create(title: 'Portrait of Dr. Gachet', artist: 'Vincent van Gogh', des
   Dr. Gachet is one of the most revered paintings by the Dutch artist Vincent van Gogh.
   It depicts Dr. Paul Gachet, a homeopathic doctor and artist
   with whom van Gogh resided following a spell in an asylum at Saint-Rémy-de-Provence."
-  creation_date: 1890 category: "Painting", price_rate: 1_000_000, style: "Post-Impressionism", user: User.rand)
+  creation_date: 1890 category: "Painting", price_rate: 1_100_000, style: "Post-Impressionism", user: User.rand,
+  image_url: "https://upload.wikimedia.org/wikipedia/commons/1/1e/Portrait_of_Dr._Gachet.jpg")
 
 
-  ArtPiece.create(title: f, artist: f, description:"Passionate about sculpture
+  ArtPiece.create(title: "L'Homme au doigt (Pointing Man)", artist: "Alberto Giacometti", description:"Passionate about sculpture
     being an act of carving, Italian sculptor Amedeo Modigliani carved Tête from a single block of
    limestone. It was sold for $59.5 million to a private collector in 2010."
-   creation_date: category: "Sculpture", price_rate: 1_000_000, style: "Surreal", user: User.rand)
+   creation_date: 1947, category: "Sculpture", price_rate: 950_000, style: "Surreal", user: User.rand,
+   image_url:"https://vsemart.com/wp-content/uploads/2015/05/L%E2%80%99homme-
+   au-doigt-Pointing-Man-sold-for-126-million-or
+   -141.3-million-with-fees-in-Christies-May-11-2015.jpg")
 
-   ArtPiece.create(title: f, artist: f, description:"Passionate about sculpture
-    being an act of carving, Italian sculptor Amedeo Modigliani carved Tête from a single block of
-   limestone. It was sold for $59.5 million to a private collector in 2010."
-   creation_date: category: "Sculpture", price_rate: 1_000_000, style: "Surreal", user: User.rand)
+   ArtPiece.create(title: "Tête", artist: 'Amedeo Modigliani', description:"Passionate about sculpture
+  being an act of carving, Italian sculptor Amedeo Modigliani carved Tête from a single block of
+  limestone. It was sold for $59.5 million to a private collector in 2010.",
+  creation_date: category: "Sculpture", price_rate: 1_000_000, style: "Surreal", user: User.rand,
+  image_url: "https://www.artnews.com/wp-content/uploads/2019/04/unnamed-1.png")
 
-
-ArtPiece.create(title: f, artist: f, description:"Passionate about sculpture
-   being an act of carving, Italian sculptor Amedeo Modigliani carved Tête from a single block of
-  limestone. It was sold for $59.5 million to a private collector in 2010."
-  creation_date: category: "Sculpture", price_rate: 1_000_000, style: "Surreal", user: User.rand)
+ArtPiece.create(title: "Balloon Dogs" , artist: "Jeff Koons", description:"Jeff Koons is renowned
+  for making grand replicas of banal, everyday objects. With a price tag of
+  $58.4 million, the Balloon Dog by Jeff Koons is the most at which any sculpture
+  from a living artist has ever sold!
+  The orange Balloon Dog is one of six dogs made of polished stainless steel in various colours.",
+  creation_date: 1994, category: "Sculpture", price_rate: 900_000, style: "Conceptual Art", user: User.rand,
+  image_url: "https://publicdelivery.org/wp-content/uploads/2017/02/Jeff-Koons-Balloon-Dog-Blue-1994
+  -high-chromium-stainless-steel-transparent-
+  color-coating-installation-view-Broad-Art-Foundation-LACMA-Los-Angeles.jpg")
