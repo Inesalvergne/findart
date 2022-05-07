dbba12eb9cd80eee9a97bfed1a6fd045e803f305
Sherman Fong <sfong.ima@gmail.com>
2022-05-07 10:40:05 -0400

    Added addresses to art_piece

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_04_010735) do
+ActiveRecord::Schema.define(version: 2022_05_07_142415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2022_05_04_010735) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image_url"
+    t.string "address"
     t.index ["user_id"], name: "index_art_pieces_on_user_id"
   end
 
@@ -54,7 +55,6 @@ ActiveRecord::Schema.define(version: 2022_05_04_010735) do
     t.string "first_name"
     t.string "last_name"
     t.text "bio"
-    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
