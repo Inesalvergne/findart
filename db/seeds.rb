e8225ccabe83f0ba692384da75678fa550770006
Ibrahim Maqsood ninjaibmaq@gmail.com <ninjaibmaq@gmail.com>
2022-05-07 12:01:47 -0400

    seed file progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,11 @@ puts 'Destruction complete!'
 puts 'Recreating stuff now...'
 User.create!(first_name: Faker::Name.first_name,
              last_name: Faker::Name.last_name,
+             address: Faker::Address.street_address,
              email: 'user1@mailmail.com', password: '1234567')
 User.create!(first_name: Faker::Name.first_name,
              last_name: Faker::Name.last_name,
+             address: Faker::Address.street_address,
              email: 'user2@mailmail.com', password: '1234567')
 
 categories = ['Paintings', 'Sculptures', 'Literature']
@@ -25,7 +27,6 @@ styles = ['Renaissance', 'Gothic', 'Baroque']
                    creation_date: Faker::Date.in_date_period,
                    category: categories.sample,
                    price_rate: (1..100).to_a.sample,
-                   address: Faker::Address.street_address,
                    style: styles.sample, user: User.first)
 end
 puts 'Recreation complete!'
@@ -34,26 +35,30 @@ puts 'Recreation complete!'
 ArtPiece.create(title: 'David', artist: 'Michelangelo', description: 'David is a masterpiece of Renaissance
   sculpture, created in marble between 1501 and 1504 by the Italian artist Michelangelo. David is a 5.17-metre
    (17 ft 0 in) marble statue of the Biblical figure David, a favoured subject in the art of Florence.',
-creation_date: 'May 1st', category: 'Sculpture', price_rate: 200_000_000, style: 'Renaissance', user: User.rand, image_url: "https://en.wikipedia.org/wiki/Salvator_Mundi_(Leonardo)#/media/File:Leonardo_da_Vinci,_Salvator_Mundi,_c.1500,_oil_on_walnut,_45.4_%C3%97_65.6_cm.jpg")
+creation_date: 'May 1st', category: 'Sculpture', price_rate:  5_000_000, style: 'Renaissance', user: User.rand,
+image_url: "https://en.wikipedia.org/wiki/David_(Michelangelo)#/media/File:Michelangelo's_David_-_right_view_2.jpg"
 
 ArtPiece.create(title: 'Salvator Mundi', artist:'Leonardo da Vinci', description:
 'The painting depicts Christ in an anachronistic blue Renaissance dress,
 making the sign of the cross with his right hand, while holding a transparent,
 non-refracting crystal orb in his left, signaling his role as Salvator Mundi
 and representing the of the heavens.', category: 'Painting', price_rate: '$2,500,000',
- style: 'Renaissance', user: User.rand, image_url: )
+ style: 'Renaissance', user: User.rand, image_url: ""https://en.wikipedia.org/wiki/Salvator_Mundi_(Leonardo)#/media/File:Leonardo_da_Vinci,_Salvator_Mundi,_c.1500,_oil_on_walnut,_45.4_%C3%97_65.6_cm.jpg")" )
 
 ArtPiece.create(title: 'The Card Players', artist: 'Paul Cézanne', description: 'The Card Players is
    a series of oil paintings
    by the French Post-Impressionist artist Paul Cézanne. Painted during Cézannes final period in the early 1890s,
   there are five paintings in the series. The versions vary in size,
    the number of players, and the setting in which the game takes place.' creation_date: 1894 category: 'Painting', price_rate: 2_000_000, style: "Post-Impressionist",
-  user: User.rand, image_url:)
+  user: User.rand, image_url: "https://en.wikipedia.org/wiki/The_Card_Players#/media/File:Les_Joueurs_de_cartes,_par_Paul_C%C3%A9zanne.jpg")
 
 ArtPiece.create(title: "The Scream", artist: "Edvard Munch", description: "The Scream is the popular
   name given to a composition created by Norwegian artist Edvard Munch in 1893. The agonized face in the painting has
-  become one of the most iconic images of art, seen as symbolizing the anxiety of the human condition."
-  creation_date: 	1893 category: , price_rate: 1_900_000, style:'Expressionist', user: User.rand, image_url:)
+  become one of the most iconic images of art, seen as symbolizing the anxiety of the human condition.",
+  creation_date: 	1893, category: 'Proto-Expressionism', price_rate: 1_900_000, style:'Expressionist', user: User.rand,
+  image_url:"https://upload.wikimedia.org/wikipedia/commons/c/c5/Edvard_Munch%2C_1893%2C_
+  The_Scream%2C_oil%2C_tempera_and_pastel_on_cardboard%2C
+  _91_x_73_cm%2C_National_Gallery_of_Norway.jpg")
 
 ArtPiece.create(title: "Portrait de l'artiste sans barbe", artist: 'Vincent van Gogh', description:"Van Gogh's dozens
   of self-portraits were an important part of his œuvre as a painter. Most probably,
@@ -61,19 +66,36 @@ ArtPiece.create(title: "Portrait de l'artiste sans barbe", artist: 'Vincent van 
   used to reproduce his face,
   i.e. his right side in the image is in reality
   the left side of his face"
-  creation_date: 1889 category: "Painting" price_rate: 1_800_000, style:"Impressionist" , user: User.rand, image_url:)
+  creation_date: 1889, category: "Painting", price_rate: 1_800_000, style:"Impressionist", user: User.rand,
+  image_url:'https://upload.wikimedia.org/wikipedia/commons/7/71/Vincent_Willem_van_Gogh_102.jpg')
 
 ArtPiece.create(title: 'Garçon à la Pipe', artist: 'Pablo Picasso', description: "Garçon à la Pipe is an oil on canvas painting by Pablo Picasso.
  It was painted in 1905 when Picasso was 24 years old, during his Rose Period, soon after he settled in the Montmartre area of Paris.
   The painting depicts a Parisian adolescent boy who holds a pipe in his left hand and wears
  a garland of flowers on his head, surrounded by two floral decorations",
- creation_date: 1905 category: 'Painting', price_rate: 1_500_000, style: "Post-Impressionism", user: User.rand, image_url:)
+ creation_date: 1905 category: 'Painting', price_rate: 1_500_000,
+ style: "Post-Impressionism", user: User.rand, image_url: "https://en
+ .wikipedia.org/wiki/Gar%C3%A7on_%C3%A0_la_pipe#/media/File:Gar%C3%A7on_%C3%A0_la_pipe.jpg")
+
+ArtPiece.create(title: 'Portrait of Dr. Gachet', artist: 'Vincent van Gogh', description:"Portrait of
+  Dr. Gachet is one of the most revered paintings by the Dutch artist Vincent van Gogh.
+  It depicts Dr. Paul Gachet, a homeopathic doctor and artist
+  with whom van Gogh resided following a spell in an asylum at Saint-Rémy-de-Provence."
+  creation_date: 1890 category: "Painting", price_rate: 1_000_000, style: "Post-Impressionism", user: User.rand)
+
+
+  ArtPiece.create(title: f, artist: f, description:"Passionate about sculpture
+    being an act of carving, Italian sculptor Amedeo Modigliani carved Tête from a single block of
+   limestone. It was sold for $59.5 million to a private collector in 2010."
+   creation_date: category: "Sculpture", price_rate: 1_000_000, style: "Surreal", user: User.rand)
+
+   ArtPiece.create(title: f, artist: f, description:"Passionate about sculpture
+    being an act of carving, Italian sculptor Amedeo Modigliani carved Tête from a single block of
+   limestone. It was sold for $59.5 million to a private collector in 2010."
+   creation_date: category: "Sculpture", price_rate: 1_000_000, style: "Surreal", user: User.rand)
+
 
 ArtPiece.create(title: f, artist: f, description:"Passionate about sculpture
    being an act of carving, Italian sculptor Amedeo Modigliani carved Tête from a single block of
   limestone. It was sold for $59.5 million to a private collector in 2010."
   creation_date: category: "Sculpture", price_rate: 1_000_000, style: "Surreal", user: User.rand)
-
-ArtPiece.create(title: "" f, artist: f, description: "L' Homme au doigt or 'Pointing Man', became the most expensive sculpture ever sold when a private collector bought it for a whopping US$141.3 million in May 2015. The 1947 bronze statue is considered Swiss sculptor Alberto Giacometti's “most iconic and evocative sculpture"" creation_date, category: df, price_rate:f, style: fd, user: User.rand)
-ArtPiece.create(title: f, artist: f, description, creation_date, category: df, price_rate:f, style: fd, user: User.rand)
-ArtPiece.create(title: f, artist: f, description, creation_date, category: df, price_rate:f, style: fd, user: User.rand)
